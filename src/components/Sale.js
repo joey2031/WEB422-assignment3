b77d9fdd11aed4e72b925432c8b6814689213af1
joey2031 <36198917+joey2031@users.noreply.github.com>
2020-02-02 14:04:06 -0500

    Added sale sales and notFound components

---
+++ src/components/Sale.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Sales from './Sales';
+class Sale extends React.Component{
+     render(){
+     return <div><h1>Sale {this.props.id}</h1></div>
+     }
+}
+export default Sales;
---
+++ src/components/Sales.js
@@ -0,0 +1,7 @@
+import React from 'react';
+class Sales extends React.Component {
+    render() {
+        return <div><h1>Sales</h1></div>
+    }
+}
+export default Sales;
