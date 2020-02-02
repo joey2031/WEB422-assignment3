98ba46dc02a0c08219c0c12de9fe6bdb8b8d1545
joey2031 <36198917+joey2031@users.noreply.github.com>
2020-02-02 14:17:38 -0500

    Added some code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {
   Navbar, Nav, NavItem, NavDropdown, MenuItem,
@@ -8,8 +7,13 @@ import {
 import { Link, Switch, Redirect, Route } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 
-function App() {
-  return (null);
+class App extends React.Component { // not sure if you need this for the App component
+  constructor(){
+    // I think this s right. Or would you put them in something like this: this.state = {}
+    this.recentlyViewed = [];
+    this.searchId = "";
+  }
+  //return (null);
 }
 
 export default App;
