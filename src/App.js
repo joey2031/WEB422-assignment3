7cac921c233c425dd59191d251afbc07a1e94b44
joey2031 <36198917+joey2031@users.noreply.github.com>
2020-02-02 15:23:31 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -8,10 +8,22 @@ import { Link, Switch, Redirect, Route } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 
 class App extends React.Component { // not sure if you need this for the App component
-  constructor(){
-    // I think this s right. Or would you put them in something like this: this.state = {}
-    this.recentlyViewed = [];
-    this.searchId = "";
+  constructor() {
+    this.state = {
+      recentlyViewed =[],
+      searchId = ""
+    }
+  }
+
+  // ASK: this method must have the value of "this" correctly bound to the function in the constructor?
+ viewSaleId(id) { 
+    if (state.recentlyViewed.indexOf(id) === -1) {
+      state.recentlyViewed.push(id);
+    }
+  }
+
+  updateSearchId(e){
+
   }
   //return (null);
 }
