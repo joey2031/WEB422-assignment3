786685ac0a9f1be21d6668c22a0118932d06452d
joey2031 <36198917+joey2031@users.noreply.github.com>
2020-02-04 08:59:32 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,17 +12,25 @@ class App extends React.Component { // not sure if you need this for the App com
     this.state = {
       recentlyViewed =[],
       searchId = ""
-    }
+    };
+    this.viewSaleId = this.viewSaleId.bind(this);
+    this.updateSearchId = this.updateSearchId.bind(this);
   }
 
   // ASK: this method must have the value of "this" correctly bound to the function in the constructor?
  viewSaleId(id) { 
     if (state.recentlyViewed.indexOf(id) === -1) {
       state.recentlyViewed.push(id);
+      // Updates the state with the updated "recentlyViewed" array, causing the component to render means call set state
+
+      this.setState((state,props)=>{
+        return {recentlyViewed: state.recentlyViewed} // override the array in the state.
+      })
     }
   }
 
   updateSearchId(e){
+    this.state.searchId = e.target.value;
 
   }
   //return (null);
