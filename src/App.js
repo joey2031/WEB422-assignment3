ff250467f4de7a3f97ba8e5432081573b92baf9d
joey2031 <36198917+joey2031@users.noreply.github.com>
2020-02-06 10:08:27 -0500

    Working on this

--- src/App.js
+++ src/App.js
@@ -18,22 +18,87 @@ class App extends React.Component { // not sure if you need this for the App com
   }
 
   // ASK: this method must have the value of "this" correctly bound to the function in the constructor?
- viewSaleId(id) { 
+  viewSaleId(id) {
     if (state.recentlyViewed.indexOf(id) === -1) {
       state.recentlyViewed.push(id);
-      // Updates the state with the updated "recentlyViewed" array, causing the component to render means call set state
 
-      this.setState((state,props)=>{
-        return {recentlyViewed: state.recentlyViewed} // override the array in the state.
+
+      // Updates the state with the updated "recentlyViewed" array, causing the component to render means call set state
+      this.setState((state, props) => {
+        return { recentlyViewed: state.recentlyViewed } // override the array in the state.
       })
     }
   }
-
-  updateSearchId(e){
+  updateSearchId(e) {
     this.state.searchId = e.target.value;
-
   }
-  //return (null);
+
+
+  render() {
+    return (
+      <div>
+        <Navbar inverse collapseOnSelect staticTop>
+          <Navbar.Header>
+            <LinkContainer to="/">
+              <Navbar.Brand>
+                WEB422 - Sales
+            </Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <LinkContainer to="/Sales">
+                <NavItem>All Sales</NavItem>
+              </LinkContainer>
+              <NavDropdown title="Previously Viewed" id="basic-nav-dropdown">
+                {this.state.recentlyViewed.length > 0 ?
+                  this.state.recentlyViewed.map((id, index) => (
+                    <LinkContainer to={`/Sale/${id}`} key={index}>
+                      <MenuItem >Sale: {id}</MenuItem>
+                    </LinkContainer>)) :
+                  <MenuItem>...</MenuItem>}
+              </NavDropdown>
+            </Nav>
+            <Navbar.Form pullRight>
+              <FormGroup>
+                <FormControl type="text" onChange={this.updateSearchId} placeholder="Sale ID" />
+              </FormGroup>{' '}
+              <Link className="btn btn-default" to={"/Sale/" + this.state.searchId}>Search</Link>
+            </Navbar.Form>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Grid>
+          <Row>
+            <Col md={12}>
+              <Switch>
+
+               <Route exact path="/" render={()=>(
+                  <Link to="/Sales"></Link> // Redirect to the /Sales route
+               )}/>
+
+               <Route exact path="/Sales" render={()=>(
+                 <Sales />
+               )}/>
+
+               <Route exact path="/Sales/:id" render={(props)=>(
+                 // Not sure if syntax is correct for viewedSale, 
+                 //also, this is taking 2 paramaters but the component only hase 1
+                <Sale id={props.match.params.id} viewedSale={this.viewSaleId()}/> 
+               )}/>
+               
+               <Route render={()=>(
+                 <NotFound />
+               )}/>
+
+              </Switch>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  };
 }
 
 export default App;
--- src/components/Sales.js
+++ src/components/Sales.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'; // will give us access to a "history" object on this.props so that we can programmatically change routes
+import {Table, Pagination} from 'react-bootstrap';
 class Sales extends React.Component {
+    constructor(props){ // They didnt tell me to but I added a constructor.
+      super(props);
+      this.state = {
+        sales: [],
+        currentPage: 1
+      }
+    }
+    
+    getData(page){
+
+    }
     render() {
         return <div><h1>Sales</h1></div>
     }
 }
-export default Sales;
+export default withRouter(Sales);
