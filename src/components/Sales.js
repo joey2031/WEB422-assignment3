95469c65ed50d83c06dc08cc037a182200c6490e
joey2031 <36198917+joey2031@users.noreply.github.com>
2020-02-10 19:18:18 -0500

    still needs some work

--- src/components/Sales.js
+++ src/components/Sales.js
@@ -1,61 +1,70 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom'; // will give us access to a "history" object on this.props so that we can programmatically change routes
-import {Table, Pagination} from 'react-bootstrap';
+import { Table, Pagination } from 'react-bootstrap';
 class Sales extends React.Component {
-    constructor(props){ 
-      super(props);
-      this.state = {
-        sales: [],
-        currentPage: 1
-      }
-      this.previousPage = this.previousPage.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      sales: [],
+      currentPage: 1
     }
-    
-    getData(page){
-      return new Promise(function(resolve, reject){
-        let httpRequest = new XMLHttpRequest();
-        httpRequest.open('GET', `https://stormy-fjord-91108.herokuapp.com/api/sales?page=${page}&perPage=10`);
-        
-        httpRequest.onreadystatechange = function(){ // I think I did this right.
-          if(httpRequest.readyState === 4){
-            if(httpRequest.status === 200){
-              resolve(JSON.parse(httpRequest.responseText));
-            } else{
-              reject("Error getting Data");
+    this.previousPage = this.previousPage.bind(this);
+  }
 
-            }
-          }
-
-        }
-
+  getData(page) {
+    return new Promise((resolve,reject)=>{
+      fetch(`https://stormy-fjord-91108.herokuapp.com/api/sales?page=${page}&perPage=10`)
+       .then(result => result.json())
+       .then((data) =>{
+         resolve(data);
+       })
+      .catch((err) => {
+        reject(err);
       });
-
-    }
-
-   componentDidMount(){ // Called after a component is mounted.
-    this.getData(this.state.currentPage)
-    .then((data)=>{
-      for(let i = 0; i < data.sales.length; i++){ // check to see if this is ok
-        this.state.sales.push(data.sales[i]);
-      }
-      this.state.sales = data.sales;
-    })
-    .catch((err)=>{
-      console.log(err);
     });
-   }
+    
+  }
 
-   previousPage(){
-     if(this.state.currentPage > 1){
-       this.getData(this.state.currentPage-1); //  the above is the case, invoke the "getData()" method with the value of (currentPage in the state - 1)
-     }
-     // not sure if/when we are suspose to use set state.
+  componentDidMount() { // Called after a component is mounted.
+    this.getData(this.state.currentPage)
+      .then((data) => {
+        console.log(data);
 
-   }
+        this.setState({ // short form
+          sales: data
+        })
 
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    render() {
-        return <div><h1>Sales</h1></div>
+  previousPage() {
+    if (this.state.currentPage > 1) {
+      this.getData(this.state.currentPage - 1).then((data) => {
+        this.setState({
+          sales: data
+        })
+      }).catch(() => { }); //  the above is the case, invoke the "getData()" method with the value of (currentPage in the state - 1)
     }
+  }
+
+
+  render() {
+  if(this.state.sales.length > 0){
+    return <div><h1>Sales</h1>
+    <ul>
+    {this.state.sales.map((sale)=>{
+      return <li key={sale._id}> {sale.customer.email} </li>
+    })}
+    </ul>
+    
+    </div>
+  }else{
+    return null; // "loading "
+  }
+    
+  }
 }
 export default withRouter(Sales);
