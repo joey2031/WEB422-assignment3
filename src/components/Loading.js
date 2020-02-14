b185afef78d48d7db335deb6fc650af59217a682
joey2031 <36198917+joey2031@users.noreply.github.com>
2020-02-14 16:42:36 -0500

    loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,10 @@
 import React from 'react';
 //import { Spinner } from 'react-bootstrap/Spinner';
-import ReactSpinner from 'react-bootstrap-spinner'
 
 class Loading extends React.Component {
     render() {
         return (
-            // <Spinner animation="border" role="status">
-            //     <span className="sr-only">Loading...</span>
-            // </Spinner>
-             <ReactSpinner type="border" color="primary" size="5" />
+            <h1>Loading...</h1>
         )
     }
 
